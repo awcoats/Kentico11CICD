04739cc3a6a6d910dcf183b0168e4895b1266eeb
Andrew Coats <andrew.coats@gmail.com>
2018-09-28 20:57:51 -0600

    call ps script.

--- etc/demo.ps1
+++ etc/demo.ps1
@@ -7,3 +7,9 @@ gci $Env:AGENT_BUILDDIRECTORY
 Write-Host "BUILD_SOURCESDIRECTORY contents:"
 gci $Env:BUILD_SOURCESDIRECTORY
 Write-Host "Over and out."
+# This updates pester not always necessary but worth noting
+Install-Module -Name Pester -Force -SkipPublisherCheck
+
+Import-Module Pester
+
+#Invoke-Pester -Script $(System.DefaultWorkingDirectory)\MyFirstModule.test.ps1 -OutputFile $(System.DefaultWorkingDirectory)\Test-Pester.XML -OutputFormat NUnitXML
