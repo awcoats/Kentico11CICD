5033fd6b0465d0a154a2413e4bad2c6d237de60a
Andrew Coats <andrew.coats@gmail.com>
2018-10-01 20:30:01 -0600

    adding pester tests.

---
+++ ci/PesterTests/Invoke-Pester.ps1
@@ -0,0 +1,25 @@
+<#
+    Invoke Pester Test from VSTS Release Task
+#>
+
+# Param([Parameter(Mandatory = $true,
+#         ValueFromPipelineByPropertyName = $true,
+#         Position = 0)]
+#     $TestScript)
+
+$TestScript=".\CI\PesterTests\demo.tests.ps1"
+
+#region Install Pester
+Write-Host "Installing Pester from PSGallery"
+Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+Install-Module -Name Pester -Force -Scope CurrentUser -SkipPublisherCheck
+#endregion
+
+#region call Pester script
+Write-Host "Calling Pester test script"
+#Invoke-Pester -Script $TestScript -PassThru
+$result = Invoke-Pester -Script $TestScript -PassThru -OutputFile ".\ci\PesterTests\Pester-TestResults.xml"  -OutputFormat LegacyNUnitXml
+if ($result.failedCount -ne 0) { 
+    Write-Error "Pester returned errors"
+}
+#endregion
